7ae9ee2b56addbfc2312ae69936959fa878d2ea8
Kipp AD <kippad@protonmail.com>
2023-01-06 17:05:44 +0000

    Add product detail view and template

--- easy_outdoor/urls.py
+++ easy_outdoor/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
-    path('', include('products.urls')),
+    path('products/', include('products.urls')),
 ]
