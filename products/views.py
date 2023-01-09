877bfff73f2e2bbaab9cb9432826c2d5e972a0eb
Kipp AD <kippad@protonmail.com>
2023-01-09 19:05:56 +0000

    Add search functionality to navbar search bar

--- products/views.py
+++ products/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Product, Category
+from django.db.models import Q
 
 
 def all_products(request):
@@ -30,6 +31,16 @@ def all_products(request):
     if 'category' in request.GET:
         category = request.GET['category']
         products = products.filter(category__name=category)
+
+    # Search functionality
+    if 'search' in request.GET:
+        search = request.GET['search']
+        if not search:
+            messages.error(request, "The search query was empty.")
+            return redirect(reverse('products'))
+
+        searches = Q(name__icontains=search) | Q(desc__icontains=search)
+        products = products.filter(searches)
 
     context = {
         'products': products,
