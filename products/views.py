03f2e13825b020f13a54f4fe116ece4d84ee4b9b
Kipp AD <kippad@protonmail.com>
2023-01-10 17:20:27 +0000

    Add product sorting functionality

--- products/views.py
+++ products/views.py
@@ -1,49 +1,73 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Product, Category
 from django.db.models import Q
+from django.db.models.functions import Lower
 
 
 def all_products(request):
     """ Displays products depending on categories selected """
     products = Product.objects.all()
+    search = None
+    sort = None
+    direction = None
 
-    # Filters all clothing items
-    if 'clothing' in request.GET:
-        products = Product.objects.filter(category__name__in=[
-            'jackets_coats',
-            't-shirts',
-            'fleeces_jumpers',
-            'trousers',
-            'socks'
-            ])
+    if request.GET:
 
-    # Filters all gear items 
-    if 'gear' in request.GET:
-        products = Product.objects.filter(category__name__in=[
-            'gloves',
-            'headwear',
-            'equipment',
-            'footwear',
-            'sunglasses'
-            ])
+        # Filters all clothing items
+        if 'clothing' in request.GET:
+            products = products.filter(category__name__in=[
+                'jackets_coats',
+                't-shirts',
+                'fleeces_jumpers',
+                'trousers',
+                'socks'
+                ])
 
-    # Filters specific category types
-    if 'category' in request.GET:
-        category = request.GET['category']
-        products = products.filter(category__name=category)
+        # Filters all gear items 
+        if 'gear' in request.GET:
+            products = products.filter(category__name__in=[
+                'gloves',
+                'headwear',
+                'equipment',
+                'footwear',
+                'sunglasses'
+                ])
 
-    # Search functionality
-    if 'search' in request.GET:
-        search = request.GET['search']
-        if not search:
-            messages.error(request, "The search query was empty.")
-            return redirect(reverse('products'))
+        # Filters specific category types
+        if 'category' in request.GET:
+            category = request.GET['category']
+            products = products.filter(category__name=category)
 
-        searches = Q(name__icontains=search) | Q(desc__icontains=search)
-        products = products.filter(searches)
+        # Search functionality
+        if 'search' in request.GET:
+            search = request.GET['search']
+            if not search:
+                messages.error(request, "The search query was empty.")
+                return redirect(reverse('products'))
+
+            searches = Q(name__icontains=search) | Q(desc__icontains=search)
+            products = products.filter(searches)
+
+        #  Sorting method taken from Code Institute Boutique Ado walkthrough project
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+            if sortkey == 'category':
+                sortkey = 'category__name'
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
+
+    sorting = f'{sort}_{direction}'
 
     context = {
         'products': products,
+        'sorting': sorting,
     }
 
     return render(request, 'products/products.html', context)
