a4310f6b3f9d4b1b8adc42d12a3060155842be79
Kipp AD <kippad@protonmail.com>
2023-01-16 17:14:44 +0000

    Add bootstrap toast message functionality

--- basket/views.py
+++ basket/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404, HttpResponse
 from products.models import Product, SizeStock
+from django.contrib import messages
 
 
 def view_basket(request):
@@ -22,20 +23,23 @@ def add_to_basket(request, product_id):
         if product_id in list(basket.keys()):
             if size_selection in basket[product_id]['size_quantities'].keys():
                 basket[product_id]['size_quantities'][size_selection] += quantity
+                messages.success(request, f'Added {quantity} {product.name} in size {size_selection.upper()} to the basket')
             else:
                 basket[product_id]['size_quantities'][size_selection] = quantity
+                messages.success(request, f'Added {quantity} {product.name} in size {size_selection.upper()} to the basket')
         else:
             basket[product_id] = {'size_quantities': {size_selection: quantity}}
+            messages.success(request, f'Added {quantity} {product.name} in size {size_selection.upper()} to the basket')
     else:
         if product_id in list(basket.keys()):
             basket[product_id] += quantity
+            messages.success(request, f'Added {product.name} to the basket')
         else:
             basket[product_id] = quantity
+            messages.success(request, f'Added {product.name} to the basket')
 
     print(basket)
-
     request.session['basket'] = basket
-
     return redirect(redirect_url)
 
 
@@ -84,8 +88,10 @@ def delete_item(request, product_id):
 
     if size_selection:
         del basket[product_id]['size_quantities'][size_selection]
+        messages.success(request, f'Removed {product.name} in size {size_selection.upper()} from the basket')
     else:
         basket.pop(product_id)
+        messages.success(request, f'Removed {product.name} from the basket')
 
     request.session['basket'] = basket
     return HttpResponse(status=200)
