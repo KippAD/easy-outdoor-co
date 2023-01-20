c6ec61ac1d628c45a20a11e16b44f1fb2f568ea3
Kipp AD <kippad@protonmail.com>
2023-01-20 18:00:09 +0000

    Prevent users from exceeding remaining stock from within checkout template

--- basket/views.py
+++ basket/views.py
@@ -6,11 +6,17 @@ from basket.contexts import basket_contents
 
 
 def view_basket(request):
+    """ Renders the basket template"""
 
     return render(request, "basket/basket.html")
 
 
 def add_to_basket(request, product_id):
+    """ 
+    Adds products to basket from product detail and also
+    prevents the adding of item quantity great than that of the
+    item's stock
+    """
     product = get_object_or_404(Product, pk=product_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
@@ -20,7 +26,6 @@ def add_to_basket(request, product_id):
 
     if product.has_sizes:
         size_selection = request.POST.get('sizes').lower()
-        # Ensures that users can't add more stock to basket than is available
         item_stock = get_object_or_404(SizeStock, product=product)
         size_stock = getattr(item_stock, size_selection)
 
@@ -47,7 +52,7 @@ def add_to_basket(request, product_id):
                 "Check to see if the remaining items are already in your basket.")
                 )
     else:
-        regular_stock = get_object_or_404(RegularStock, product=product)         
+        regular_stock = get_object_or_404(RegularStock, product=product)
         if (regular_stock.stock - quantity) >= 0:
             if product_id in list(basket.keys()):
                 if (basket[product_id] + quantity) <= regular_stock.stock:
@@ -79,10 +84,19 @@ def update_quantity(request, product_id):
     size_selection = None
     if 'product_size' in request.POST:
         size_selection = request.POST.get('product_size')
+
     if size_selection:
+        item_stock = get_object_or_404(SizeStock, product=product)
+        size_stock = getattr(item_stock, size_selection)
         if 'increment' in request.POST:
-            basket[product_id]['size_quantities'][size_selection] += 1
-            messages.success(request, f'Changed {product.name} quantity')
+            if (basket[product_id]['size_quantities'][size_selection] + 1) <= size_stock:
+                basket[product_id]['size_quantities'][size_selection] += 1
+                messages.success(request, f'Changed {product.name} quantity')
+            else:
+                messages.error(request, (
+                    f"You cannot add any more quantity to your order. "
+                    "You have all the remaining stock of this size in your basket.")
+                    )
         elif 'decrement' in request.POST:
             if basket[product_id]['size_quantities'][size_selection] > 1:
                 basket[product_id]['size_quantities'][size_selection] -= 1
@@ -91,9 +105,16 @@ def update_quantity(request, product_id):
                 del basket[product_id]['size_quantities'][size_selection]
                 messages.success(request, f'Removed {product.name} in size {size_selection.upper()} from the basket')
     else:
+        regular_stock = get_object_or_404(RegularStock, product=product)
         if 'increment' in request.POST:
-            basket[product_id] += 1
-            messages.success(request, f'Changed {product.name} quantity')
+            if (basket[product_id] + 1) <= regular_stock.stock:
+                basket[product_id] += 1
+                messages.success(request, f'Changed {product.name} quantity')
+            else:
+                messages.error(request, (
+                    f"You cannot add any more quantity to your order. "
+                    "You have all the remaining stock of this size in your basket.")
+                    )
         elif 'decrement' in request.POST:
             if basket[product_id] > 1:
                 basket[product_id] -= 1
@@ -130,27 +151,5 @@ def delete_item(request, product_id):
     except Exception as e:
         messages.error(request, f'Error removing item from shopping basket: {e}')
         return HttpResponse(status=500)
-
-
-# Check stock function
-
-def check_size_stock(basket, product, stock_type, quantity, size):
-    """Function to ensure that size stocks caan't be exceeded"""
-    available = None
-    item_stock = get_object_or_404(SizeStock, product=product)
-    size_stock = getattr(item_stock, size)
-    product_id = str(product.id)
-
-    if product_id in basket.keys():
-        if basket[product_id]['size_quantities'][size] >= size_stock:
-            available = False
-    else:
-        if (size_stock - quantity) < 0:
-            available = False
-        else:
-            available = True
-
-    return available
-
 
 
