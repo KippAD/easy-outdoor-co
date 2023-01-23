b07bd4b8ec6752e117abe1dad9f48fdf8332cdfa
Kipp AD <kippad@protonmail.com>
2023-01-23 17:02:29 +0000

    Replace incorrect img urls on home page

--- easy_outdoor/settings.py
+++ easy_outdoor/settings.py
@@ -202,7 +202,7 @@ if 'USE_AWS' in os.environ:
 
     # Overrides static and media URLs in production
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
-    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}.s3.eu-west-3.amazonaws.com/{MEDIAFILES_LOCATION}/'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
