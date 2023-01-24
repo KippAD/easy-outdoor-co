45a57835519a4018bf1f09dbbc3fdfda5e66640e
Kipp AD <kippad@protonmail.com>
2023-01-24 20:44:45 +0000

    Add frequently asked questions page

--- home/urls.py
+++ home/urls.py
@@ -4,5 +4,6 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('', views.HomePage, name='home')
+    path('', views.index, name='home'),
+    path('frequently-asked/', views.frequently_asked_questions, name='frequently-asked-questions'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- home/views.py
+++ home/views.py
@@ -4,14 +4,17 @@ from products.models import Product
 import random
 
 
-# Taken from https://levelup.gitconnected.com/django-customize-404-error-page-72c6b6277317
-def HomePage(request):
+def index(request):
+    """Displays home page"""
     products = list(Product.objects.all())
-
     random_products = random.sample(products, k=8)
-    
     context = {
         'related_products': random_products,
     }
 
     return render(request, 'home/index.html', context)
+
+
+def frequently_asked_questions(request):
+    """Displays faq page"""
+    return render(request, 'home/faq.html')
