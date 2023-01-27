07192d31267ce7f5ff1fe5a68d49f9e76c3ad7f8
Kipp AD <kippad@protonmail.com>
2023-01-27 23:01:58 +0000

    Add links to footer and small style adjustments to various templates

--- newsletter/views.py
+++ newsletter/views.py
@@ -24,7 +24,7 @@ def newsletter_subscribe(request):
                 email_content = f"Hi {name}! This is a confirmation email for your recent \
                     subscription to the Easy Outdoor Co. newsletter.\
                         Email:{email}"
-                html_message = render_to_string('newsletter/email-template.html', {
+                html_message = render_to_string('newsletter/newsletter-template.html', {
                     'subject': email_subject,
                     'content': email_content
                     })
